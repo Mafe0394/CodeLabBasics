11377ec8b56eed72c11752436a423bb0b2662a66
Mafe Vargas <mfvargasm0394@gmail.com>
2021-09-02 19:58:38 -0400

    Flexible layout

--- app/src/main/java/com/projects/codelabbasics/MainActivity.kt
+++ app/src/main/java/com/projects/codelabbasics/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,18 +39,24 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
-    val counterState= remember{ mutableStateOf(0)}
-    Column {
-        for (name in names) {
-            Greeting(name = name)
+fun MyScreenContent(names: List<String> = List(1000){ "Android #$it"}) {
+    val counterState = remember { mutableStateOf(0) }
+    Column(modifier = Modifier.fillMaxHeight(1f)) {
+        NameList(names = names, modifier = Modifier.weight(1f))
+        Counter(count = counterState.value,
+            updateCount = {
+                counterState.value = it
+            })
+    }
+}
+
+@Composable
+fun NameList(names:List<String>,modifier:Modifier){
+    LazyColumn(modifier = modifier) {
+        items(items = names){
+            Greeting(name = it)
             Divider(color = Color.Black)
         }
-        Divider(color = Color.Transparent,thickness = 32.dp)
-        Counter(count = counterState.value,
-        updateCount = {
-            counterState.value=it
-        })
     }
 }
 
@@ -64,7 +71,7 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun Counter(count:Int,updateCount:(Int)->Unit) {
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
 //    var count by remember {
 //        mutableStateOf(1)
 //    }
@@ -75,8 +82,11 @@ fun Counter(count:Int,updateCount:(Int)->Unit) {
 //        Text("Clicked ${count.value} times")
 //    }
     Button(onClick = {
-        updateCount(count+1)
-    }) {
+        updateCount(count + 1)
+    },
+    colors = ButtonDefaults.buttonColors(
+        if(count>5) MaterialTheme.colors.primary else MaterialTheme.colors.secondary
+    )) {
         Text("Clicked ${count} times")
     }
 }
